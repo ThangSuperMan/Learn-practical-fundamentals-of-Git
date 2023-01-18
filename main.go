78b297e517bfd977d85da4771f79cba5c85e1538
thangsuperman <thangjenny2002@gmail.com>
2023-01-18 22:16:19 +0700

    feat: added the sum function and print the result itself

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("hello")
 	fmt.Println("modified")
+	result := sum(1, 2)
+	fmt.Println("result", result)
 }
